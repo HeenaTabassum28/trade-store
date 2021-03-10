b0498f7cdfb76e0a030ceaebacb4f6458b1d5456
test <test@lit03ina6.apac.sas.com>
2021-03-10 16:53:24 +0530

    Initial Commit

---
+++ src/test/java/com/deutschebank/trade/store/TradeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deutschebank.trade.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
